8726ecc3f1bcaa7c816cf34f0e376f5322444cd4
jan.garaj <jan.garaj@13ac1171-822b-0410-820f-b71bd62439dd>
2007-03-22 15:27:22 +0000

    strukltura emailu

---
+++ trunk/email.h
@@ -0,0 +1,8 @@
+typedef struct {
+	char *head;		//hlavicka emailu
+	char *body;		//telo emailu
+	char hash;		//hash tela emailu
+	char filesystem;//filesystem, kde sa ma ulozit email
+	int hardlink;	//ulozit ako hardlinku
+} email;
+

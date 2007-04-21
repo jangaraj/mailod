1977d3465611a80506a61aee8e0e03ffef8cb2e7
jan.garaj <jan.garaj@13ac1171-822b-0410-820f-b71bd62439dd>
2007-04-21 16:25:07 +0000

    autocommit Sat Apr 21 18:25:01 CEST 2007

--- trunk/autogen.sh
+++ trunk/autogen.sh
@@ -1,5 +1,27 @@
 #!/bin/sh
 #http://mij.oltrelinux.com/devel/autoconf-automake/
+
+#1 CLEANING - vsetky veci del
+
+#2 AUTOSCAN
+#autoscan
+
+#3 MOVE to configure.ac
+#mv configure.scan configure.ac
+
+#4 VIM configure.ac
+#AC_INIT(mailod, 0.1, garaj@fei.sk)
+#AC_CONFIG_SRCDIR([src/mailod.c])
+
+#5 AUTOCONF
+#autoconf
+
+#6 VIM Makefile.am
+#AUTOMAKE_OPTIONS = foreign
+#SUBDIRS = src
+
+#7
+
 #echo Running autoscan
 #autoscan
 #echo Moving configure.scan to configure.ac
@@ -8,12 +30,20 @@
 #AC_INIT(mailod, 0.1, garaj@fei.sk)  >> configure.ac
 #AM_INIT_AUTOMAKE(mailod, 0.1)
 
-#echo Running aclocal.
-#aclocal -I m4 $ACLOCAL_FLAGS
 
+#echo Running aclocal.
+#aclocal
+
+
+#vytvorit configure.h.in
 echo Running autoheader.
 autoheader
+
+
 echo Running automake.
-automake -a
+automake --add-missing
+
+
+#finaly script configure
 echo Running autoconf
 autoconf

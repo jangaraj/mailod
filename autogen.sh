709fc3b9d820c5d53eedffa24237376f12631e29
jan.garaj <jan.garaj@13ac1171-822b-0410-820f-b71bd62439dd>
2007-04-21 17:25:07 +0000

    autocommit Sat Apr 21 19:25:01 CEST 2007

--- autogen.sh
+++ autogen.sh
@@ -20,7 +20,32 @@
 #AUTOMAKE_OPTIONS = foreign
 #SUBDIRS = src
 
-#7
+#7 VIM src/Makefile.am
+## what flags you want to pass to the C compiler & linker
+#AM_CFLAGS = --pedantic -Wall -std=c99 -O2 -g
+#AM_LDFLAGS =
+#
+##this lists the binaries to produce, the (non-PHONY, binary) targets in
+##the previous manual Makefile
+#bin_PROGRAMS = xmailod
+#xmailod_SOURCES = mailod.c const.h database_function.h email.h hash_function.h common_function.h logging.h
+
+#8 VIM configure.ac
+#AC_INIT(mailod, 0.1, garaj@fei.sk)
+#AM_INIT_AUTOMAKE(mailod, 0.1)
+#AC_CONFIG_FILES([src/Makefile])
+##NIE#AC_OUTPUT(Makefile src/Makefile)
+
+#9 ACLOCAL
+
+#10 AUTOHEADER
+
+#11 automake --add-missing
+
+#12 autoconf
+
+
+
 
 #echo Running autoscan
 #autoscan

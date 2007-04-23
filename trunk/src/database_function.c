7f239fec69fb5c651fe7b3ffefb298f11d1a7ac1
jan.garaj <jan.garaj@13ac1171-822b-0410-820f-b71bd62439dd>
2007-04-23 00:25:06 +0000

    autocommit Mon Apr 23 02:25:01 CEST 2007

--- trunk/src/database_function.c
+++ trunk/src/database_function.c
@@ -45,3 +45,41 @@ email *select_by_hash(config *conf, char hash_value[]) {
 	dbi_shutdown();
     return ident_email;
 }
+
+int insert_email(config *conf, email *new_email)
+{
+	dbi_conn conn;
+    dbi_result result;
+	email *ident_email;
+	char sql[1024];  //TODO betonovana konstanta 1024
+
+	dbi_initialize(NULL);
+    conn = dbi_conn_new(conf->db_driver);
+    dbi_conn_set_option(conn, "host", conf->db_host);
+    dbi_conn_set_option(conn, "username", conf->db_username);
+	dbi_conn_set_option(conn, "password", conf->db_password);
+	dbi_conn_set_option(conn, "dbname", conf->db_name);
+	//dbi_conn_set_option(conn, "encoding", "UTF-8");
+    if (dbi_conn_connect(conn) < 0) {
+    	fprintf(stderr,"Could not connect. Please check the option settings\n");
+	return 1;
+    }
+    else {
+		//TODO TRANSAKCIE
+		sprintf(sql,"INSERT body_hash`='%s' AND `timestamp`>(NOW()+INTERVAL -%d MINUTE) AND `number_hardlinks`<'1000' ORDER BY `email_id` LIMIT 1",new_email->hash, conf->time_window);
+		//printf("SQL: %s\n",sql);
+	   	result = dbi_conn_query(conn,sql);
+    	if (result) {
+			if((dbi_result_get_numrows(result))<1) return 1;		//0 selected rows
+			//TODO otestovat ci sa vlozilo OK
+			while (dbi_result_next_row(result)) {
+	  			//ident_email->hardlink = dbi_result_get_uint(result, "number_hardlinks");
+  				ident_email->filesystem = (char *) dbi_result_get_string(result, "filesystem");
+			}
+			dbi_result_free(result);
+		}
+		dbi_conn_close(conn);
+	}
+	dbi_shutdown();
+	return 0;
+}
--- trunk/src/database_function.h
+++ trunk/src/database_function.h
@@ -1,1 +1,2 @@
-email *select_by_hash(config *conf, char hash_value[]); 
+email *select_by_hash(config *conf, char hash_value[]);
+int insert_email(config *conf, email *new_email);

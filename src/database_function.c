b2d0e4fdf3d6209073ecb9babb0139761f94aaef
jan.garaj <jan.garaj@13ac1171-822b-0410-820f-b71bd62439dd>
2007-05-04 11:18:21 +0000

    autocommit Fri May  4 13:25:01 CEST 2007

--- src/database_function.c
+++ src/database_function.c
@@ -9,13 +9,10 @@
 #include "common_function.h"
 #include "database_function.h"
 
-email *select_by_hash(config *conf, char hash_value[]) {
+dbi_conn connect_db(config *conf)
+{
 	dbi_conn conn;
-    dbi_result result;
-	email *ident_email;
-	char sql[1024];  //TODO betonovana konstanta 1024
 
-	ident_email=NULL;
 	dbi_initialize(NULL);
     conn = dbi_conn_new(conf->db_driver);
     dbi_conn_set_option(conn, "host", conf->db_host);
@@ -26,11 +23,44 @@ email *select_by_hash(config *conf, char hash_value[]) {
     if (dbi_conn_connect(conn) < 0) {
     	fprintf(stderr,"Could not connect. Please check the option settings\n");
 		return NULL;
-    }
+	}
+
+	return conn;
+}
+
+int disconnect_db(dbi_conn conn)
+{
+	dbi_conn_close(conn);
+	dbi_shutdown();
+
+	return 0;
+}
+
+email *select_by_hash(dbi_conn conn, char hash_value[], int time_window) {
+	//dbi_conn conn;
+    dbi_result result;
+	email *ident_email;
+	char sql[1024];  //TODO betonovana konstanta 1024
+
+	ident_email=NULL;
+/*	dbi_initialize(NULL);
+    conn = dbi_conn_new(conf->db_driver);
+    dbi_conn_set_option(conn, "host", conf->db_host);
+    dbi_conn_set_option(conn, "username", conf->db_username);
+	dbi_conn_set_option(conn, "password", conf->db_password);
+	dbi_conn_set_option(conn, "dbname", conf->db_name);
+	dbi_conn_set_option(conn, "encoding", "UTF-8");
+    if (dbi_conn_connect(conn) < 0) {
+    	fprintf(stderr,"Could not connect. Please check the option settings\n");
+		return NULL;
+    }*/
+	if(0) {
+		printf("Tu nikdy nebudem\n");
+	}
     else {
 		//TODO TRANSAKCIE - su potrebne,
 		//selekujem vzdy najstarsi mozny
-		sprintf(sql,"SELECT * FROM `mailod` WHERE `body_hash`='%s' AND `timestamp`>(NOW()+INTERVAL -%d MINUTE) ORDER BY `email_id` LIMIT 1;",hash_value, conf->time_window);
+		sprintf(sql,"SELECT * FROM `mailod` WHERE `body_hash`='%s' AND `timestamp`>(NOW()+INTERVAL -%d MINUTE) ORDER BY `email_id` LIMIT 1;",hash_value, time_window);
 		//printf("selectSQL: %s\n",sql);
 	   	result = dbi_conn_query(conn,sql);
     	if (result) {
@@ -47,19 +77,19 @@ email *select_by_hash(config *conf, char hash_value[]) {
 			}
 			dbi_result_free(result);
 		}
-		dbi_conn_close(conn);
+//		dbi_conn_close(conn);
 	}
-	dbi_shutdown();
+//	dbi_shutdown();
     return ident_email;
 }
 
-int insert_email(config *conf, email *new_email)
+int insert_email(dbi_conn conn, email *new_email)
 {
-	dbi_conn conn;
+//	dbi_conn conn;
     dbi_result result;
 	char sql[1024];  //TODO betonovana konstanta 1024
 
-	dbi_initialize(NULL);
+/*	dbi_initialize(NULL);
     conn = dbi_conn_new(conf->db_driver);
     dbi_conn_set_option(conn, "host", conf->db_host);
     dbi_conn_set_option(conn, "username", conf->db_username);
@@ -69,7 +99,10 @@ int insert_email(config *conf, email *new_email)
     if (dbi_conn_connect(conn) < 0) {
     	fprintf(stderr,"Could not connect. Please check the option settings\n");
 		return 1;
-    }
+    }*/
+	if(0) {
+		printf("Tu nikdy nebude\n");
+	}
     else {
 		//TODO TRANSAKCIE
 		//START TRANSACTION;
@@ -82,19 +115,19 @@ int insert_email(config *conf, email *new_email)
 	   	result = dbi_conn_query(conn,sql);
 		if(dbi_result_get_numrows_affected(result)!=1) return 1;   // 0 affected rows - not inserted
 		dbi_result_free(result);
-		dbi_conn_close(conn);
+//		dbi_conn_close(conn);
 	}
-	dbi_shutdown();
+//	dbi_shutdown();
 	return 0;
 }
 
-int delete_email(config *conf, email *ident_email)
+int delete_email(dbi_conn conn, email *ident_email)
 {
-	dbi_conn conn;
+//	dbi_conn conn;
     dbi_result result;
 	char sql[1024];  //TODO betonovana konstanta 1024
 
-	dbi_initialize(NULL);
+/*	dbi_initialize(NULL);
     conn = dbi_conn_new(conf->db_driver);
     dbi_conn_set_option(conn, "host", conf->db_host);
     dbi_conn_set_option(conn, "username", conf->db_username);
@@ -104,7 +137,10 @@ int delete_email(config *conf, email *ident_email)
     if (dbi_conn_connect(conn) < 0) {
     	fprintf(stderr,"Could not connect. Please check the option settings\n");
 		return 1;
-    }
+    }*/
+	if(0) {
+		printf("Tu nikdy nebudem\n");
+	}
     else {
 		//TODO TRANSAKCIE
 		//START TRANSACTION;
@@ -117,8 +153,8 @@ int delete_email(config *conf, email *ident_email)
 	   	result = dbi_conn_query(conn,sql);
 		if(dbi_result_get_numrows_affected(result)!=1) return 1;   // 0 affected rows - not inserted
 		dbi_result_free(result);
-		dbi_conn_close(conn);
+		//dbi_conn_close(conn);
 	}
-	dbi_shutdown();
+//	dbi_shutdown();
 	return 0;
 }

c3c5e9c6e225ab0ba6b758c8de457fd692582dd2
jan.garaj <jan.garaj@13ac1171-822b-0410-820f-b71bd62439dd>
2007-05-06 22:56:50 +0000

    90% done, pridane nastavenia postfixu + init script mailod-u

--- src/sql_database.sql
+++ src/sql_database.sql
@@ -2,8 +2,7 @@ CREATE TABLE `mailod` (
   `email_id` int(10) NOT NULL auto_increment,
   `body_hash` varchar(64) NOT NULL default '',
   `body_length` varchar(10) NOT NULL default '',
-  `filesystem` varchar(255) NOT NULL default '',
+  `inode` int(10) NOT NULL default '0',
   `timestamp` timestamp(14) NOT NULL,
-  `number_hardlinks` int(5) NOT NULL default '0',
    PRIMARY KEY  (`email_id`)
 ) TYPE=MyISAM AUTO_INCREMENT=1 ;

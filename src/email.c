d7ed2539706c3f43795e0098fd66c16d90efcce6
jan.garaj <jan.garaj@13ac1171-822b-0410-820f-b71bd62439dd>
2007-05-06 15:18:05 +0000

    autocommit Sun May  6 17:25:01 CEST 2007

--- src/email.c
+++ src/email.c
@@ -12,6 +12,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <acl/libacl.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <dirent.h>
@@ -113,6 +114,7 @@ email *readmail(int input)
 	}
 	else {
 		read_email->homedir = getpwnam(reading_email_to)->pw_dir;
+		read_email->to_uid = getpwnam(reading_email_to)->pw_uid;
 	}
 	read_email->done = 1;
 
@@ -124,6 +126,9 @@ int write_email(email *new_email)
 	int fw, i;
 	struct stat filestat;
 	mode_t umask_bak;
+	acl_t acl;					/* ACL information */
+	acl_entry_t entry_p;		/* ACL entry */	
+	acl_permset_t permset;		/* Permissions */
 
 	if((new_email->filepath = make_filepath(new_email)) == NULL) {
 		logging(DEBUG,"Error, generate uniq name of email file\n");
@@ -170,7 +175,36 @@ int write_email(email *new_email)
 	//restore umask
 	umask(umask_bak);
 	new_email->inode = filestat.st_ino;
-
+	//setting acl
+	acl = acl_get_file(new_email->filepath,ACL_TYPE_ACCESS);
+	if(!acl) {
+		logging(DEBUG,"In file %s not found acl's\n");
+	}
+	if(acl_create_entry(&acl,&entry_p) == -1) {
+		logging(DEBUG,"Error, adding acl entry: %s\n",strerror(errno));
+	}
+	if(acl_get_permset(entry_p, &permset) == -1) {
+		logging(DEBUG,"Error, getting permset of acl: %s\n",strerror(errno));
+	}
+	if(acl_add_perm(permset, ACL_READ) == -1) {
+		logging(DEBUG,"Error, adding permset of acl: %s\n",strerror(errno));
+	}
+	if(acl_set_tag_type(entry_p, ACL_USER) == -1) {
+		logging(DEBUG,"Error, setting type of acl: %s\n",strerror(errno));
+	}
+	if(acl_set_qualifier(entry_p, &new_email->to_uid) == -1) {
+		logging(DEBUG,"Error, seting qualifier of acl: %s\n",strerror(errno));
+	}
+	if(acl_set_permset(entry_p, permset) == -1) {
+		logging(DEBUG,"Error, seting permset of acl: %s\n",strerror(errno));	
+	}
+	if (!acl_valid(acl)) {
+		logging(DEBUG,"Error, not valid acl\n");
+	}
+	if(acl_set_file(new_email->filepath,ACL_TYPE_ACCESS,acl) == -1) {
+		logging(DEBUG,"Error, set new acl for file %s\n",new_email->filepath);
+//		if(errno == EINVAL) new_email->done = 2;
+	}
 	return 0;
 }
 

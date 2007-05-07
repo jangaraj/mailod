8c8b8510ab258b6a6fc1f393a3e0f2e1fb9fa58e
jan.garaj <jan.garaj@13ac1171-822b-0410-820f-b71bd62439dd>
2007-05-07 23:29:02 +0000

    backup pred zmenou struktury db a db funkcii

--- trunk/src/database_function.c
+++ trunk/src/database_function.c
@@ -22,7 +22,6 @@ dbi_conn connect_db(config *conf)
 	dbi_conn_set_option(conn, "dbname", conf->db_name);
 	dbi_conn_set_option(conn, "encoding", "UTF-8");
     if (dbi_conn_connect(conn) < 0) {
-//    	fprintf(stderr,"Could not connect. Please check the option settings\n");
 		logging(DEBUG,"Could not connect. Please check the option settings\n");
 		return NULL;
 	}
@@ -45,16 +44,13 @@ email *select_by_hash(dbi_conn conn, char hash_value[], int time_window)
 	char sql[1024];  //TODO betonovana konstanta 1024
 
 	ident_email=NULL;
-	//TODO TRANSAKCIE - su potrebne,
 	//selekujem vzdy najstarsi mozny
 	sprintf(sql,"SELECT * FROM `mailod` WHERE `body_hash`='%s' AND `timestamp`>(NOW()+INTERVAL -%d MINUTE) ORDER BY `email_id` LIMIT 1;",hash_value, time_window);
-	//printf("selectSQL: %s\n",sql);
 	logging(DEBUG,"selectSQL: %s\n",sql);
    	result = dbi_conn_query(conn,sql);
    	if (result) {
 		if((dbi_result_get_numrows(result))<1) return NULL;		//0 selected rows
 		if((ident_email=(email *) malloc(sizeof(email)))==NULL) {
-//			fprintf(stderr, "Error, mallock ident_email\n");
 			logging(DEBUG,"Error, mallock ident_email\n");
 			return NULL;
 		}
@@ -73,15 +69,8 @@ int insert_email(dbi_conn conn, email *new_email)
 {
     dbi_result result;
 	char sql[1024];  //TODO betonovana konstanta 1024
-
-	//TODO TRANSAKCIE
-	//START TRANSACTION;
-	//SELECT @A:=SUM(salary) FROM table1 WHERE type=1;
-	//UPDATE table2 SET summary=@A WHERE type=1;
-	//COMMIT;
 	//TODO nazvy tabuliek a stlpcov do const.h
 	sprintf(sql,"INSERT INTO mailod (body_hash, body_length, filepath, inode) VALUES (\"%s\",%d,\"%s\",%ld);", new_email->hash,new_email->size, new_email->filepath, new_email->inode);
-	//printf("insertSQL: %s\n",sql);
    	result = dbi_conn_query(conn,sql);
 	if(dbi_result_get_numrows_affected(result)!=1) return 1;   // 0 affected rows - not inserted
 	dbi_result_free(result);
@@ -93,15 +82,8 @@ int delete_email(dbi_conn conn, email *ident_email)
 {
     dbi_result result;
 	char sql[1024];  //TODO betonovana konstanta 1024
-
-	//TODO TRANSAKCIE
-	//START TRANSACTION;
-	//SELECT @A:=SUM(salary) FROM table1 WHERE type=1;
-	//UPDATE table2 SET summary=@A WHERE type=1;
-	//COMMIT;
 	//TODO nazvy tabuliek a stlpcov do const.h
-	sprintf(sql,"DELETE FROM mailod where `email_id`=\"%ld\");", ident_email->id);
-	//printf("deleteSQL: %s\n",sql);
+	sprintf(sql,"DELETE FROM mailod where `email_id`=\"%ld\";", ident_email->id);
    	result = dbi_conn_query(conn,sql);
 	if(dbi_result_get_numrows_affected(result)!=1) return 1;   // 0 affected rows - not inserted
 	dbi_result_free(result);
